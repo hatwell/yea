a62bbdc0861f65cf57dd2118a9309f50b591d890
hatwell <caroline.hatwell@gmail.com>
2017-06-15 12:21:18 +0100

    added react front end

---
+++ front_end/client/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.addEventListener('load', () => {
+  const targetDiv = document.getElementById('app');
+  ReactDOM.render(<h1>hello</h1>, targetDiv);
+});
