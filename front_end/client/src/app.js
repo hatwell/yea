22765bda151d72e359658c508f2e42572eeb9ed6
hatwell <caroline.hatwell@gmail.com>
2017-06-15 12:26:59 +0100

    deleted front end because y not

--- front_end/client/src/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-window.addEventListener('load', () => {
-  const targetDiv = document.getElementById('app');
-  ReactDOM.render(<h1>hello</h1>, targetDiv);
-});
