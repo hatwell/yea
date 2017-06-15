a7ef02261857de54d89c64dba1e81756b1e26c14
hatwell <caroline.hatwell@gmail.com>
2017-06-15 15:26:32 +0100

    authentication set up

--- yea_api/config/routes.rb
+++ yea_api/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :installs
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   resources :products
   root 'products#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
