0a64d0f3b3fc8262de6deb8c010a105e34cd0129
hatwell <caroline.hatwell@gmail.com>
2017-06-21 18:35:56 +0100

    pushing to git

---
+++ yea_front/client/src/components/QuizHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Paper from 'material-ui/Paper'
+import Toolbar from 'material-ui/Toolbar'
+
+class Header extends React.Component {
+
+  render(){
+      return (
+        <Toolbar className = "color-highlight header-bar">
+          <h2 className="app-title">w a t</h2>
+       </Toolbar>
+    )
+  }
+
+}
+
+export default Header
